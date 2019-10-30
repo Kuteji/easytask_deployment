81b49f2e079a78d3cebca2c039661d9457a2afd5
Kuteji <talaverae4@gmail.com>
2019-10-30 02:24:12 -0600

    reestablecimiento de puerto

--- config/email.js
+++ config/email.js
@@ -1,4 +1,4 @@
 export const user = 'talaverae4@gmail.com';
 export const pass = 'KriptacartogenesisKuteji32';
 export const host = 'smtp.gmail.com';
-export const port = '2525';
+export const port = '587';
